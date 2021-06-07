8b7ed573ce65a8f2adfa8c17915a6a13379bb7c7
Gabriel Thomé <gabrielallan.thome@gmail.com>
2021-06-07 19:12:06 -0300

    revert last

--- app/api/server/v1/groups.js
+++ app/api/server/v1/groups.js
@@ -560,7 +560,7 @@ API.v1.addRoute('groups.messages', { authRequired: true }, {
 API.v1.addRoute('groups.online', { authRequired: true }, {
 	get() {
 		const { query } = this.parseJsonQuery();
-		const ourQuery = Object.assign({}, query, { t: 'd' });
+		const ourQuery = Object.assign({}, query, { t: 'p' });
 
 		const room = Rooms.findOne(ourQuery);
 
